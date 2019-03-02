1423821bbaaa3342386fcf9cc1a3d04c6603f08e
Dnny <mcmillandanny@gmail.com>
2019-03-01 23:11:37 -0500

    styling up webpages

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,52 +1,12 @@
-'use strict';
+"use strict";
 
 console.log('RXBAR');
 
-// let ingredientsImages = document.querySelectorAll('.showing-class');
-// let currentImgIndex = 0;
-// let playTotal = 6;
-// let playCount = 0;
+var hamburger = document.querySelector(".menu-lines");
+var nav = document.querySelector(".closed-nav");
 
-
-// setInterval(() => {
-//     // animateTextInOut()
-//     playCount++;
-//     if (playCount >= playTotal) {
-//         return;
-//     }
-
-//     let hideShowingText = function() {
-//         TweenMax.to( ingredientsImages[currentImgIndex], 0.3, { 
-//             display: "none", 
-//             opacity: 0,
-//             x: 30,
-//         })
-//     }
-
-//     let showNewText = function() {
-//         TweenMax.fromTo( ingredientsImages[currentImgIndex], 0.3, {
-//             display: "none", 
-//             opacity: 0,
-//             x: -30,
-//             y: 0,
-//         }, { 
-//             display: "block", 
-//             opacity: 1,
-//             x: 0,
-//             y: 0,
-//             delay: 0.3
-//          })
-
-//     }   
-
-//     let next = function() {
-//         hideShowingText()
-//         currentImgIndex++
-//         if (currentImgIndex === ingredientsImages.length) {
-// 			currentImgIndex = 0;
-// 		}
-// 		showNewText()
-//     }
-//     next()
-// }, 1000)
+hamburger.addEventListener("click", function () {
+    nav.classList.toggle('open-nav');
+    console.log("clciked");
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,10 @@
 console.log('RXBAR')
 
-// let ingredientsImages = document.querySelectorAll('.showing-class');
-// let currentImgIndex = 0;
-// let playTotal = 6;
-// let playCount = 0;
+let hamburger = document.querySelector(".menu-lines");
+let nav = document.querySelector(".closed-nav")
 
 
-// setInterval(() => {
-//     // animateTextInOut()
-//     playCount++;
-//     if (playCount >= playTotal) {
-//         return;
-//     }
-
-//     let hideShowingText = function() {
-//         TweenMax.to( ingredientsImages[currentImgIndex], 0.3, { 
-//             display: "none", 
-//             opacity: 0,
-//             x: 30,
-//         })
-//     }
-
-//     let showNewText = function() {
-//         TweenMax.fromTo( ingredientsImages[currentImgIndex], 0.3, {
-//             display: "none", 
-//             opacity: 0,
-//             x: -30,
-//             y: 0,
-//         }, { 
-//             display: "block", 
-//             opacity: 1,
-//             x: 0,
-//             y: 0,
-//             delay: 0.3
-//          })
-       
-//     }   
-
-//     let next = function() {
-//         hideShowingText()
-//         currentImgIndex++
-//         if (currentImgIndex === ingredientsImages.length) {
-// 			currentImgIndex = 0;
-// 		}
-// 		showNewText()
-//     }
-//     next()
-// }, 1000)
+hamburger.addEventListener("click", ()=> {
+    nav.classList.toggle('open-nav');
+    console.log("clciked");
+})
