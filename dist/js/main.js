35ca14377fb4270fbd8b6573b79185851b5a7b6c
Dnny <mcmillandanny@gmail.com>
2019-03-03 15:09:05 -0500

    animated hamburger in/out

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,9 +4,56 @@ console.log('RXBAR');
 
 var hamburger = document.querySelector(".menu-lines");
 var nav = document.querySelector(".closed-nav");
+var closeBtn = document.querySelector('.close-btn');
+var navAnimate = document.querySelector('.nav');
 
 hamburger.addEventListener("click", function () {
-    nav.classList.toggle('open-nav');
-    console.log("clciked");
+
+    TweenMax.fromTo(nav, .7, {
+        width: "0%",
+        height: "0%",
+        x: 30,
+        y: 30,
+        borderRadius: 100
+
+    }, {
+        borderRadius: 0,
+        width: "100%",
+        height: "100%",
+        x: 0,
+        y: 0,
+        display: "block"
+    });
+
+    TweenMax.fromTo(navAnimate, 2.3, {
+        opacity: 0
+    }, {
+        opacity: 1
+    });
+
+    TweenMax.to(closeBtn, .1, {
+        opacity: 1
+    });
+});
+
+closeBtn.addEventListener("click", function () {
+    console.log('clicked');
+
+    TweenMax.to(nav, .7, {
+        width: "0%",
+        height: "0%",
+        x: 60,
+        y: 40,
+        borderRadius: 100,
+        display: "none"
+    });
+
+    TweenMax.to(navAnimate, .1, {
+        opacity: 0
+    });
+
+    TweenMax.to(closeBtn, .1, {
+        opacity: 0
+    });
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,62 @@
 console.log('RXBAR')
 
 let hamburger = document.querySelector(".menu-lines");
-let nav = document.querySelector(".closed-nav")
+let nav = document.querySelector(".closed-nav");
+let closeBtn = document.querySelector('.close-btn');
+let navAnimate = document.querySelector('.nav');
 
 
 hamburger.addEventListener("click", ()=> {
-    nav.classList.toggle('open-nav');
-    console.log("clciked");
+
+    TweenMax.fromTo(nav, .7, {
+        width: "0%",
+        height: "0%",
+        x: 30,
+        y: 30,
+        borderRadius: 100,
+
+    }, 
+    {
+        borderRadius: 0,
+        width: "100%",
+        height: "100%",
+        x: 0,
+        y: 0,
+        display: "block"
+    })
+
+    TweenMax.fromTo(navAnimate, 2.3, {
+        opacity: 0,
+    },
+    {
+        opacity: 1
+    })
+
+    TweenMax.to(closeBtn, .1, {
+        opacity: 1
+    })
+})
+
+closeBtn.addEventListener("click", () => {
+    console.log('clicked');
+
+    TweenMax.to(nav, .7, {
+        width: "0%",
+        height: "0%",
+        x: 60,
+        y: 40,
+        borderRadius: 100,
+        display: "none"
+    })
+
+    TweenMax.to(navAnimate, .1, {
+        opacity: 0
+    })
+
+    TweenMax.to(closeBtn, .1, {
+        opacity: 0
+    })
+
+
+
 })
